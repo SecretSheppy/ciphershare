32539dffa3aa3077d799cb1845a7f8e9372ec09e
Alex McLeish <47481225+AxNoodle@users.noreply.github.com>
2024-11-16 18:28:08 +0000

    feat: File upload now exists

--- handlers/upload.go
+++ handlers/upload.go
@@ -3,7 +3,17 @@ package handlers
 import (
 	"fmt"
 	"golang-encrypted-filesharing/cryptography"
+	"io"
+	"mime/multipart"
 	"net/http"
+	"os"
+	"path/filepath"
+	"runtime"
+)
+
+var (
+	_, b, _, _ = runtime.Caller(0)
+	RootPath   = filepath.Join(filepath.Dir(b), "../")
 )
 
 func (h *Handlers) Upload(w http.ResponseWriter, r *http.Request) {
@@ -16,14 +26,25 @@ func (h *Handlers) Upload(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handlers) UploadFile(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
+	err := r.ParseMultipartForm(500 << 20)
 	if err != nil {
 		h.log.Error(err.Error())
 	} else {
-		h.log.Info("File is being uploaded")
+		h.log.Info("Form is being parse")
 	}
-	fmt.Println(r.FormValue("emails"))
-	fmt.Println(r.FormValue("fileUpload"))
+
+	file, handler, err := r.FormFile("fileUpload")
+	if err != nil {
+		h.log.Error(err.Error())
+	} else {
+		h.log.Info("File is being parsed")
+	}
+	err = saveFile(file, handler, getFileName())
+	if err != nil {
+		h.log.Error(err.Error())
+	} else {
+		h.log.Info("File has been downloaded")
+	}
 
 	// Encrypt the file
 	key, encryptedFile := cryptography.Encrypt(r.FormValue("fileUpload"))
@@ -34,4 +55,24 @@ func (h *Handlers) UploadFile(w http.ResponseWriter, r *http.Request) {
 
 	// Upload emails to database
 
+}
+
+func getFileName() string {
+	return "name"
+}
+
+func saveFile(file multipart.File, handler *multipart.FileHeader, filename string) error {
+	defer file.Close()
+	folderPath := fmt.Sprintf("%s%s", RootPath, "/files")
+	tempFile, err := os.CreateTemp(folderPath, filename)
+	if err != nil {
+		return err
+	}
+	defer tempFile.Close()
+	filebytes, err := io.ReadAll(file)
+	if err != nil {
+		return err
+	}
+	tempFile.Write(filebytes)
+	return nil
 }
