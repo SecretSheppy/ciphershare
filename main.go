f62683d657c99e91dd6fdc38c8e76eca31f4fe59
OMWalmsley <119448765+OMWalmsley@users.noreply.github.com>
2024-11-16 16:08:01 +0000

    chore!: First iteration of getting stuff from the database

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"golang-encrypted-filesharing/handlers"
 	"html/template"
+	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -16,6 +24,43 @@ const (
 )
 
 func main() {
+	//database stuff
+	//if err := godotenv.Load(); err != nil {
+	//	log.Fatal("Error loading .env file")
+	//}
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("MONGODB_URI environment variable not set")
+	}
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+	coll := client.Database("security_go").Collection("go_project")
+	pathToEncryptedFile := "pathname"
+	var result bson.M
+	err = coll.FindOne(context.TODO(), bson.D{{"pathToEncryptedFile", pathToEncryptedFile}}).
+		Decode(&result)
+
+	if errors.Is(err, mongo.ErrNoDocuments) {
+		fmt.Printf("No document was found with the name %s\n", pathToEncryptedFile)
+		return
+	}
+	if err != nil {
+		panic(err)
+	}
+	jsonData, err := json.MarshalIndent(result, "", "    ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(jsonData))
+
+	//Other stuff
 	tpl := template.Must(template.New("").ParseGlob("./templates/*.gohtml"))
 
 	h := handlers.NewHandlers(tpl)
@@ -32,8 +77,8 @@ func main() {
 		ReadTimeout:  15 * time.Second,
 	}
 
-	err := srv.ListenAndServeTLS(certificate, key)
-	if err != nil {
+	nerr := srv.ListenAndServeTLS(certificate, key)
+	if nerr != nil {
 		fmt.Println(err)
 	}
 }
