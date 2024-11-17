fd8f9b34acb7b14fa9592da079f2ca7f1f2e275b
SecretSheppy <62794249+SecretSheppy@users.noreply.github.com>
2024-11-17 00:05:13 +0000

    feat: added copied to clipboard success message

--- static/js/clipboard-copy.js
+++ static/js/clipboard-copy.js
@@ -1,17 +1,32 @@
 'use strict'
+
+/**
+ * displays the success message in the DOM
+ */
+function displaySuccessMessage() {
+    $('#copied-success-message').show();
+
+    setTimeout(() => {
+        $('#copied-success-message').fadeOut();
+    }, 500);
+}
+
+/**
+ * Puts the text into the users clipboard
+ *
+ * @param {string} valueToCopy the string to put into the clipboard
+ */
+function putTextInClipboard(valueToCopy) {
+    navigator.clipboard.writeText(valueToCopy)
+        .then(r => {
+            console.log('Text copied to clipboard:', valueToCopy);
+            displaySuccessMessage();
+        }).catch(e => console.error('failed to copy text:', e));
+}
+
 $(() => {
     $('#copy-to-clipboard').on( 'click', ()=> {
         putTextInClipboard($('#link-to-copy').attr('href'));
     });
 
 });
-
-/**
- * Puts the text into the users clipboard
- * @param valueToCopy the string to put into the clipboard
- */
-function putTextInClipboard(valueToCopy) {
-    navigator.clipboard.writeText(valueToCopy)
-        .then(r => console.log('Text copied to clipboard:', valueToCopy))
-        .catch(e => console.log('failed to copy text:', e));
-}
